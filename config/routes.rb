8c0784addd2a84db3ccdfa8d81c9bab32fe2cdab
longcn-1045 <chu.ngoc.long@sun-asterisk.com>
2021-02-24 14:58:38 +0700

    Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   get 'static_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+
+  resources :users, only: %i(new create show)
 end

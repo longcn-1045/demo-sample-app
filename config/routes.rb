ebf92053bcab1483c6bf154f98552f5b1b993184
longcn-1045 <chu.ngoc.long@sun-asterisk.com>
2021-03-09 15:10:12 +0700

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root "static_pages#home"
 
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get "static_pages/home"
+  get "static_pages/help"
 
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   resources :users, only: %i(new create show)
 end
